696d47cad0700839e2b4eb3e28fc62f2b49bec12
Ahmad Kheirandish <40075811+ahmadkh1995@users.noreply.github.com>
2019-07-06 14:58:29 +0200

    Add files via upload

---
+++ PCL_ProbRobotic/build/CMakeFiles/PCL_ProbRobotic.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PCL_ProbRobotic.dir/main.cpp.o"
+  "PCL_ProbRobotic.pdb"
+  "PCL_ProbRobotic"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PCL_ProbRobotic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
