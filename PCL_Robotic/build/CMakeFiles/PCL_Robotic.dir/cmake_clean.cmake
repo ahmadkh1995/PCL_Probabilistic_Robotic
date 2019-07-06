60e7c934bb1766124eca4543143185fea162ebf8
Ahmad Kheirandish <40075811+ahmadkh1995@users.noreply.github.com>
2019-07-06 18:37:48 +0200

    Add Visualize viewer codes

---
+++ PCL_Robotic/build/CMakeFiles/PCL_Robotic.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "PCL_Robotic_autogen"
+  "CMakeFiles/PCL_Robotic_autogen.dir/AutogenOldSettings.cmake"
+  "CMakeFiles/PCL_Robotic.dir/main.cpp.o"
+  "CMakeFiles/PCL_Robotic.dir/PCL_Robotic_autogen/mocs_compilation.cpp.o"
+  "PCL_Robotic.pdb"
+  "PCL_Robotic"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PCL_Robotic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ PCL_Robotic/build/CMakeFiles/PCL_Robotic_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "PCL_Robotic_autogen"
+  "CMakeFiles/PCL_Robotic_autogen.dir/AutogenOldSettings.cmake"
+  "CMakeFiles/PCL_Robotic_autogen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/PCL_Robotic_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
