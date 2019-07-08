64d7d6b382f05953882e6b342b5f013aadf566b5
Ahmad Kheirandish <40075811+ahmadkh1995@users.noreply.github.com>
2019-07-08 23:50:16 +0200

    Add files via upload

--- PCL_Robotic/main.cpp
+++ PCL_Robotic/main.cpp
@@ -1,45 +1,70 @@
-#include <QCoreApplication>
-#include <pcl/visualization/cloud_viewer.h>
 #include <iostream>
-#include <pcl/io/io.h>
 #include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/registration/icp.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/io/io.h>
 
 int user_data;
 
-void
-viewerOneOff (pcl::visualization::PCLVisualizer& viewer)
+
+
+int
+ main (int argc, char** argv)
 {
-   
-    std::cout << "i only run once" << std::endl;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_out (new pcl::PointCloud<pcl::PointXYZ>);
 
-}
-
-
-
-int main ()
-{
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::io::loadPCDFile ("globe.pcd", *cloud);
-    pcl::visualization::CloudViewer viewer("Globe in Scene");
-    viewer.showCloud(cloud);  //blocks until the cloud is actually rendered
-
-   //pcl::PointCloud<pcl::PointXYZ>::Ptr cloud2 (new pcl::PointCloud<pcl::PointXYZ>);
-   // pcl::io::loadPCDFile ("globe.pcd", *cloud2);
-   // pcl::visualization::CloudViewer viewer2("Cloud Viewer 2");
-   // viewer2.showCloud(cloud2);  //blocks until the cloud is actually rendered
+ pcl::io::loadPCDFile ("globe.pcd", *cloud_in);
+ pcl::io::loadPCDFile ("globe_in_scene.pcd", *cloud_out);
 
 
-    //This will only get called once
-    viewer.runOnVisualizationThreadOnce (viewerOneOff);
+  
+  std::cout << "size:" << cloud_out->points.size() << std::endl;
+  for (size_t i = 0; i < cloud_in->points.size (); ++i)
+    cloud_out->points[i].x = cloud_in->points[i].x + 0.7f;
+  std::cout << "Transformed " << cloud_in->points.size () << " data points:"
+      << std::endl;
+  for (size_t i = 0; i < cloud_out->points.size (); ++i)
+    std::cout << "    " << cloud_out->points[i].x << " " <<
+      cloud_out->points[i].y << " " << cloud_out->points[i].z << std::endl;
+  pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
+  icp.setInputSource(cloud_in);
+  icp.setInputTarget(cloud_out);
+  pcl::PointCloud<pcl::PointXYZ> Final;
+  icp.align(Final);
+  std::cout << "\nhas converged(status):" << icp.hasConverged() << " \nscore: " <<
+  icp.getFitnessScore() << std::endl;
+  std::cout<<"Transformation Matrix:"<<endl;
+  std::cout << icp.getFinalTransformation() << std::endl;
+
+
+
+pcl::io::savePCDFileASCII ("transform_pcd.pcd", Final);
+  std::cerr << "Saved " << Final.points.size () << " data points to transform_pcd.pcd." << std::endl;
+    
+    
+    pcl::PointCloud<pcl::PointXYZ>::Ptr Final1 (new pcl::PointCloud<pcl::PointXYZ>);
+
+pcl::io::loadPCDFile ("transform_pcd.pcd", *Final1);
+    pcl::visualization::CloudViewer viewer1("Transformed");
+  viewer1.showCloud(Final1);  //blocks until the cloud is actually rendered
+
+  //This will only get called once
 
     //This will get called once per visualization iteration
    
-    while (!viewer.wasStopped ())
+    while (!viewer1.wasStopped ())
     {
     //you can also do cool processing here
     //FIXME: Note that this is running in a separate thread from viewerPsycho
     //and you should guard against race conditions yourself...
     user_data++;
     }
-    return 0;
+
+
+
+
+
+ return (0);
 }
